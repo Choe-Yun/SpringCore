250cb1153b4ce4434384e9f07c67ef4cce528d18
Choe-Yun <cjh7687@gmail.com>
2021-05-21 12:32:22 +0900

    Initial commit

---
+++ src/test/java/springproject/springcore/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package springproject.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
